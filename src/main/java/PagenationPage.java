babd9929e9c6045d535ceb9dbc26b74226ecca66
funak <funak.attila@gmail.com>
2022-06-07 10:19:52 +0200

    commit

--- src/main/java/PagenationPage.java
+++ src/main/java/PagenationPage.java
@@ -1,2 +1,51 @@
-package PACKAGE_NAME;public class PageingPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class PagenationPage {
+
+    WebDriver driver;
+
+    private final String url = "https://demo.seleniumeasy.com/table-pagination-demo.html";
+
+    private final By nextLink = By.xpath("//*[@class=\"next_link\"]");
+
+    private final By tableRowVisible = By.xpath("//*[@style=\"display: table-row;\"]");
+
+    ////*[@style="display: table-row;"]
+
+
+    public PagenationPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void navigate() {
+        driver.navigate().to(url);
+    }
+
+    public int numberOfRows() {
+        int num = 0;
+
+        List<WebElement> rows = driver.findElements(tableRowVisible);
+        num = rows.size();
+
+        return num;
+    }
+
+
+    public boolean isLastPage(){
+        String attr = driver.findElement(nextLink).getAttribute("style");
+        return attr.equals("display: none;");
+    }
+
+    public void clickNext() {
+        if (!isLastPage()) {
+            driver.findElement(nextLink).click();
+        }
+    }
+
+
+
 }
--- src/test/java/TestPagenation.java
+++ src/test/java/TestPagenation.java
@@ -1,2 +1,57 @@
-package PACKAGE_NAME;public class TestPagenation {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestPagenation {
+
+    WebDriver driver;
+
+    @BeforeEach
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-notifications");
+        options.addArguments("--disable-extensions");
+        //options.addArguments("--headless");
+        options.addArguments("--window-size=1920,1080");
+        options.addArguments("start-maximized");
+
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void pagenationTest() {
+        PagenationPage pagenationPage = new PagenationPage(driver);
+        pagenationPage.navigate();
+
+        int actual = 0;
+
+        while (true) {
+            actual += pagenationPage.numberOfRows();
+
+            if (pagenationPage.isLastPage()) {
+                break;
+            }
+            pagenationPage.clickNext();
+        }
+
+
+        Assertions.assertEquals(13, actual);
+
+
+    }
+
+
+
 }
